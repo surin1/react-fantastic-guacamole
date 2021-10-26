84aa9fa58fe6b1cc3e884c6c1c3d4afaaf341fd8
Ivan Surin <ivan@musiio.com>
2021-10-27 07:49:29 +0800

    modules exports

---
+++ index.js
@@ -0,0 +1,15 @@
+import { useMusicPlayer } from "./src/hooks/index";
+import CurrentTime from "./src/components/CurrentTime";
+import Duration from "./src/components/Duration";
+import PlayPauseButton from "./src/components/PlayPauseButton";
+import SeekSlider from "./src/components/SeekSlider";
+import VideoFrame from "./src/components/VideoFrame";
+
+export {
+  useMusicPlayer,
+  CurrentTime,
+  Duration,
+  PlayPauseButton,
+  SeekSlider,
+  VideoFrame,
+};
--- webpack.config.js
+++ webpack.config.js
@@ -32,10 +32,6 @@ const config = {
         loader: "ts-loader",
         exclude: ["/node_modules/"],
       },
-      // {
-      //   test: /\.css$/,
-      //   use: ["style-loader", "css-loader"],
-      // },
       {
         test: /\.css$/i,
         use: [
